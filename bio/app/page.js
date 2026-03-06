02ebee7ae9a7c8b310b408714b8809dcd59eb830
joao0cb <joao.00000853774@unicap.br>
2026-03-06 12:02:11 -0300

    Mini Bio Joao

--- bio/app/page.js
+++ bio/app/page.js
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div>
-      <p>oi!</p>
+    <div className="fundo">
+      <div className="divisaoVert">
+        <img src="/fotojoao.jpg" alt="Foto de João Victor" className="foto"></img>
+      </div>
+      <div className="divHor">
+        <div>
+          <p className="nome">João Victor</p>
+        </div>
+        <div>
+          <p className="bio">Meu nome é João Victor, tenho 20 anos e sou estudante de Ciência da Computação. Gosto muito de assistir filmes e séries, jogar, ir à praia,
+            ir ao cinema e jogar vôlei, apesar de não ser muito bom nisso.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
