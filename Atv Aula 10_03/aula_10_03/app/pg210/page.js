97d900564288b39093b36a071e2f9ac8b71ceb33
joao0cb <joao.00000853774@unicap.br>
2026-03-10 20:31:41 -0300

    Atividade Completa Aula 10_03

---
+++ Atv Aula 10_03/aula_10_03/app/pg210/page.js
@@ -0,0 +1,9 @@
+import styles from "./pg210_module.css";
+
+export default function Pg210() {
+  return (
+    <div className="foto">
+      <h1 className="frase">Primeiro exercício CSS</h1>
+    </div>
+  );
+}
---
+++ Atv Aula 10_03/aula_10_03/app/pg263/page.js
@@ -0,0 +1,55 @@
+import styles from "./pg263_module.css";
+
+export default function Pg210() {
+  return (
+    <main>
+        <div className="principal">
+            <div className="cards">
+                <div className="foto">
+                    <img src="/adidasliteracer.webp"></img>
+                </div>
+                <div className="info">                    
+                    <p>Tênis Adidas Lite Racer 4 0</p>
+                    <p>R$180,49</p>
+                </div>
+            </div>
+            <div className="cards">
+                <div className="foto">                    
+                    <img src="/adidasgrandcourt.webp"></img>
+                </div>
+                <div className="info">                    
+                    <p>Tênis Masculino Adidas Grand Court Base</p>
+                    <p>R$179,99</p>
+                </div>
+            </div>
+            <div className="cards">
+                <div className="foto">                    
+                    <img src="/adidasbreaknetpreto.webp"></img>
+                </div>
+                <div className="info">                    
+                    <p>Tênis Adidas Breaknet Preto</p>
+                    <p>R$179,99</p>
+                </div>
+            </div>
+            <div className="cards">
+                <div className="foto">                    
+                    <img src="/adidasbreaknetoff.webp"></img>
+                </div>
+                <div className="info">                    
+                    <p>Tênis Adidas Breaknet Off White</p>
+                    <p>R$179,99</p>
+                </div>
+            </div>
+            <div className="cards">
+                <div className="foto">
+                    <img src="/adidasvlcourt.avif"></img>
+                </div>
+                <div className="info">
+                    <p>Tênis Adidas VL Court 3.0 Collegiate</p>
+                    <p>R$249,99</p>
+                </div>
+            </div>
+        </div>
+    </main>
+  );
+}
