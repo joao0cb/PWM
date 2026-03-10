5afada847bb09fa17e391cd5201a8351e1f893be
joao0cb <joao.00000853774@unicap.br>
2026-03-10 20:43:23 -0300

    abort

--- Atv Aula 10_03/aula_10_03/app/pg210/page.js
+++
@@ -1,9 +0,0 @@
-import styles from "./pg210_module.css";
-
-export default function Pg210() {
-  return (
-    <div className="foto">
-      <h1 className="frase">Primeiro exercício CSS</h1>
-    </div>
-  );
-}
