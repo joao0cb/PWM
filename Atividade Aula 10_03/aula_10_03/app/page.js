1d8a7a6f38efbcf80edefa5a492e635a07df5e6f
joao0cb <joao.00000853774@unicap.br>
2026-03-10 21:21:11 -0300

    att final

---
+++ Atividade Aula 10_03/aula_10_03/app/page.js
@@ -0,0 +1,17 @@
+import styles from "./page_module.css";
+
+export default function Home() {
+  return (
+    <div className="fundo">
+      <p>
+        Atividade Reposição Aula 10/03/2026
+      </p>
+      <ul>
+        <li><a href="/pg210">Exércicio Página 210</a></li>
+        <li><a href="/pg231">Exércicio Página 231</a></li>
+        <li><a href="/pg246">Exércicio Página 246</a></li>
+        <li><a href="/pg263">Exércicio Página 263</a></li>
+      </ul>
+    </div>
+  );
+}
---
+++ Atividade Aula 10_03/aula_10_03/app/pg246/page.js
@@ -0,0 +1,13 @@
+import styles from "./pg246_module.css";
+
+export default function Pg210() {
+  return (
+    <main>
+        <p>
+            Oi, meu nome é João Victor tenho 20 anos e vi 164 filmes em 2026!
+        </p>
+        <a href="https://letterboxd.com/joaoo_cb/">Meu letterboxd</a>
+        <a href="https://letterboxd.com/film/la-la-land/">Melhor filme do mundo</a>
+    </main>
+  );
+}
