dcc7f999915f177f64dd7e144bef7dee72549bd5
joao0cb <joao.00000853774@unicap.br>
2026-03-10 21:53:30 -0300

    correcao nome da pasta

--- atv_aula_10_03/app/pg246/page.js
+++ atv_aula_10_03/app/pg246/page.js
@@ -4,7 +4,7 @@ export default function Pg210() {
   return (
     <main>
         <p>
-            Oi, meu nome é João Victor tenho 20 anos e vi 164 filmes em 2026!
+            Oi, meu nome é João Victor tenho 20 anos e vi 164 filmes em 2025!
         </p>
         <a href="https://letterboxd.com/joaoo_cb/">Meu letterboxd</a>
         <a href="https://letterboxd.com/film/la-la-land/">Melhor filme do mundo</a>
